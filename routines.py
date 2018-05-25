1dd89327cfc0c28e44ba247742596916bcd490d2
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-05-25 14:18:30 -0600

    testing plotting routines

--- routines.py
+++ routines.py
@@ -981,7 +981,7 @@ def plot_covariance_overlay_from_list_of_folder(folder_list,inputfolder,
 
 
 
-    ax_pos_RSS.legend(loc = "upper center",bbox_to_anchor = (0.5,1.1),ncol = ncol)  
+    ax_pos_RSS.legend(loc = "upper center",bbox_to_anchor = (0.5,1.2),ncol = ncol)  
              
     if outputname is not None:
         plt.savefig(outputname,bbox_inches='tight')
